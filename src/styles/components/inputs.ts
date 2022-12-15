21802c91b29c63e743d4ea07f8742e62d8427483
Matheus Popolin <matheuspopolin@hotmail.com>
2022-12-15 18:12:20 -0300

    feat: create global styled components

---
+++ src/styles/components/inputs.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { css } from "styled-components";
+
+export const StyledLabel = styled.label`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+
+  align-self: start;
+
+  color: var(--color-grey-300);
+`;
+
+export const StyledInput = styled.input`
+  width: 100%;
+  max-width: 452px;
+  height: 60px;
+
+  padding: 0px 15px;
+
+  background-color: var(--color-grey-50);
+
+  border-radius: var(--radius-1);
+
+  color: var(--color-grey-600);
+
+  ::placeholder {
+    color: var(--color-grey-100);
+  }
+
+  :focus {
+    background-color: var(--color-grey-0);
+    border-color: var(--color-grey-600);
+  }
+
+  /* ${({ color }) => {
+    if (color) {
+      return css`
+        border: 1px solid var(--color-negative);
+        :focus {
+          border-color: var(--color-negative);
+        }
+      `;
+    }
+  }} */
+`;
---
+++ src/styles/global.ts
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Global = createGlobalStyle`
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Inter', sans-serif;
+    font-style: normal;
+    }
+
+    body{
+        background-color: var(--color-grey-4);
+    }
+
+    button{
+        border: none;
+        background-color: transparent;
+        cursor: pointer;
+    }
+
+    img {
+    max-width: 100%;
+    }
+
+    ul {
+    list-style: none;
+    }
+
+    :root {
+    /* Primary Palette */
+    --color-primary:   #27AE60;
+    --color-secondary: #EB5757;
+
+    /* Grey Scale Palette */
+    --color-grey-600: #333333;
+    --color-grey-300: #828282;
+    --color-grey-100: #E0E0E0;
+    --color-grey-50:  #F5F5F5;
+    --color-grey-0:   #FFFFFF;
+    --color-grey-opacity: rgba(51, 51, 51, 0.5);
+    
+    /* Feedback Palette */
+    --color-negative:    #E60000;
+    --color-warning:     #FFCD07;
+    --color-sucess:      #168821;
+    --color-information: #155BCB;
+
+    --radius-1: 8px;
+    --radius-2: 4px;
+
+    /* Toast Palette */
+    --toastify-color-light:   #F5F5F5;  
+    --toastify-color-error:   #E60000;   
+    --toastify-color-warning: #FFCD07;  
+    --toastify-color-success: #168821;  
+    --toastify-color-info:    #155BCB;   
+    }    
+`;
